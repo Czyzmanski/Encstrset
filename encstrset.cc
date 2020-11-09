330859026425d5ef2e0030d4aec99936a166ea98
Szymon <szymon.czyzmanski@gmail.com>
2020-11-09 19:29:14 +0100

    Replaced repeating string literals with function wrappers

--- encstrset.cc
+++ encstrset.cc
@@ -21,7 +21,7 @@ namespace {
     using val_func_t = void (*)(unsigned long, const string &);
 
     using encrypted_t = std::unordered_map<unsigned long, std::unordered_set<string>>;
-    using info_pair_t = std::pair<string, string>;
+    using msg_pair_t = std::pair<string, string>;
     using val_func_pair_t = std::pair<val_func_t, val_func_t>;
     using res_pair_t = std::pair<bool, bool>;
 
@@ -38,6 +38,14 @@ namespace {
     encrypted_t &encrypted() {
         static encrypted_t encrypted;
         return encrypted;
+    }
+
+    inline string set_id_msg() {
+        return "set #";
+    }
+
+    inline string set_not_present_msg() {
+        return " does not exist";
     }
 
     inline void print_func_call_if_debug(const string &func_name,
@@ -64,17 +72,17 @@ namespace {
         }
     }
 
-    inline void print_set_info_if_debug(const string &func_name,
-                                        unsigned long id, const string &info) {
+    inline void print_set_msg_if_debug(const string &func_name,
+                                       unsigned long id, const string &msg) {
         if (debug) {
-            cerr() << func_name << ": " << "set #" << id << info << endl;
+            cerr() << func_name << ": " << set_id_msg() << id << msg << endl;
         }
     }
 
-    inline void print_func_info_if_debug(const string &func_name,
-                                         const string &info) {
+    inline void print_func_msg_if_debug(const string &func_name,
+                                        const string &msg) {
         if (debug) {
-            cerr() << func_name << info << endl;
+            cerr() << func_name << msg << endl;
         }
     }
 
@@ -155,48 +163,48 @@ namespace {
     }
 
     void handle_set_operation(const string &func_name, unsigned long id,
-                              const info_pair_t &info_pair,
+                              const msg_pair_t &msg_pair,
                               set_func_t func_if_set_present) {
         print_func_call_if_debug(func_name, id);
 
         if (is_set_present(id)) {
             func_if_set_present(id);
-            print_set_info_if_debug(func_name, id, info_pair.first);
+            print_set_msg_if_debug(func_name, id, msg_pair.first);
         }
         else {
-            print_set_info_if_debug(func_name, id, info_pair.second);
+            print_set_msg_if_debug(func_name, id, msg_pair.second);
         }
     }
 
     bool handle_value_operation(const string &func_name, unsigned long id,
                                 const char *value, const char *key,
-                                const info_pair_t &info_pair,
+                                const msg_pair_t &msg_pair,
                                 const val_func_pair_t val_func_pair,
                                 const res_pair_t res_pair) {
         print_func_call_if_debug(func_name, id,
                                  string_repr(value), string_repr(key));
 
         if (value == nullptr) {
-            print_func_info_if_debug(func_name, ": invalid value (NULL)");
+            print_func_msg_if_debug(func_name, ": invalid value (NULL)");
             return false;
         }
         else if (!is_set_present(id)) {
-            print_set_info_if_debug(func_name, id, " does not exist");
+            print_set_msg_if_debug(func_name, id, set_not_present_msg());
             return false;
         }
         else {
             string new_value = encrypt_value(value, key);
 
-            stringstream info;
-            info << ", " << cypher(new_value);
+            stringstream msg;
+            msg << ", " << cypher(new_value);
 
             if (is_value_present(id, new_value)) {
                 if (val_func_pair.first != nullptr) {
                     val_func_pair.first(id, new_value);
                 }
 
-                info << info_pair.first;
-                print_set_info_if_debug(func_name, id, info.str());
+                msg << msg_pair.first;
+                print_set_msg_if_debug(func_name, id, msg.str());
 
                 return res_pair.first;
             }
@@ -205,8 +213,8 @@ namespace {
                     val_func_pair.second(id, new_value);
                 }
 
-                info << info_pair.second;
-                print_set_info_if_debug(func_name, id, info.str());
+                msg << msg_pair.second;
+                print_set_msg_if_debug(func_name, id, msg.str());
 
                 return res_pair.second;
             }
@@ -224,28 +232,28 @@ namespace jnp1 {
         std::unordered_set<string> new_set;
         encrypted().insert({added_sets(), new_set});
 
-        print_set_info_if_debug("encstrset_new", added_sets(), " created");
+        print_set_msg_if_debug("encstrset_new", added_sets(), " created");
 
         return added_sets()++;
     }
 
     void encstrset_delete(unsigned long id) {
         handle_set_operation("encstrset_delete", id,
-                             {" deleted", " does not exist"}, erase_set);
+                             {" deleted", set_not_present_msg()}, erase_set);
     }
 
     size_t encstrset_size(unsigned long id) {
         print_func_call_if_debug("encstrset_size", id);
 
         if (!is_set_present(id)) {
-            print_set_info_if_debug("encstrset_size", id, " does not exist");
+            print_set_msg_if_debug("encstrset_size", id, set_not_present_msg());
             return 0;
         }
 
-        stringstream info;
-        info << " contains " << encrypted()[id].size() << " element(s)";
+        stringstream msg;
+        msg << " contains " << encrypted()[id].size() << " element(s)";
 
-        print_set_info_if_debug("encstrset_size", id, info.str());
+        print_set_msg_if_debug("encstrset_size", id, msg.str());
 
         return encrypted()[id].size();
     }
@@ -270,35 +278,35 @@ namespace jnp1 {
 
     void encstrset_clear(unsigned long id) {
         handle_set_operation("encstrset_clear", id,
-                             {" cleared", " does not exist"}, clear_set);
+                             {" cleared", set_not_present_msg()}, clear_set);
     }
 
     void encstrset_copy(unsigned long src_id, unsigned long dst_id) {
         print_func_call_if_debug("encstrset_copy", src_id, dst_id);
 
         if (!is_set_present(src_id)) {
-            print_set_info_if_debug("encstrset_copy", src_id, " does not exist");
+            print_set_msg_if_debug("encstrset_copy", src_id, set_not_present_msg());
         }
         else if (!is_set_present(dst_id)) {
-            print_set_info_if_debug("encstrset_copy", dst_id, " does not exist");
+            print_set_msg_if_debug("encstrset_copy", dst_id, set_not_present_msg());
         }
         else {
             for (const string &s : encrypted()[src_id]) {
                 bool added = encrypted()[dst_id].insert(s).second;
 
-                stringstream info;
-                info << ": ";
+                stringstream msg;
+                msg << ": ";
 
                 if (added) {
-                    info << cypher(s) << " copied from " << "set #"
-                         << src_id << " to " << "set #" << dst_id;
+                    msg << cypher(s) << " copied from " << set_id_msg()
+                        << src_id << " to " << set_id_msg() << dst_id;
                 }
                 else {
-                    info << "copied " << cypher(s)
-                         << " was already present in " << "set #" << dst_id;
+                    msg << "copied " << cypher(s)
+                        << " was already present in " << set_id_msg() << dst_id;
                 }
 
-                print_func_info_if_debug("encstrset_copy", info.str());
+                print_func_msg_if_debug("encstrset_copy", msg.str());
             }
         }
     }
